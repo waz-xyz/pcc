6b182ea0cbdbbcfdb1133f4a1c584e3401f9f7a4
ragge <ragge>
2014-07-03 14:25:51 +0000

    Do not change type on named pointers, since they are memory addresses. Fixes Jira#PCC-440 by Fred J. Tydeman

--- arch/amd64/local.c
+++ arch/amd64/local.c
@@ -1,4 +1,4 @@
-/*	$Id: local.c,v 1.79 2014/06/29 16:03:55 ragge Exp $	*/
+/*	$Id: local.c,v 1.80 2014/07/03 14:25:51 ragge Exp $	*/
 /*
  * Copyright (c) 2008 Michael Shalayeff
  * Copyright (c) 2003 Anders Magnusson (ragge@ludd.luth.se).
@@ -388,6 +388,9 @@ clocal(NODE *p)
 
 		if (o == ICON) {
 			CONSZ val = l->n_lval;
+
+			if (ISPTR(l->n_type) && !nncon(l))
+				break; /* cannot convert named pointers */
 
 			if (!ISPTR(m)) /* Pointers don't need to be conv'd */
 			    switch (m) {
