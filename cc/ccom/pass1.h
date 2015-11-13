f0471ee048acac8a3fba31c8f16da831bfafe865
ragge <ragge>
2015-11-13 12:47:09 +0000

    Do not create FCON nodes in scan.l. Fixes one of the bugs in parsing numbers after if statement.

--- cc/ccom/pass1.h
+++ cc/ccom/pass1.h
@@ -1,4 +1,4 @@
-/*	$Id: pass1.h,v 1.287 2015/11/13 11:33:14 ragge Exp $	*/
+/*	$Id: pass1.h,v 1.288 2015/11/13 12:47:09 ragge Exp $	*/
 /*
  * Copyright(C) Caldera International Inc. 2001-2002. All rights reserved.
  *
@@ -303,8 +303,8 @@ OFFSZ	tsize(TWORD, union dimfun *, struct attr *),
 P1ND *	typenode(P1ND *new);
 void	spalloc(P1ND *, P1ND *, OFFSZ);
 char	*exname(char *);
-P1ND	*floatcon(char *);
-P1ND	*fhexcon(char *);
+union flt *floatcon(char *);
+union flt *fhexcon(char *);
 P1ND	*bdty(int op, ...);
 extern struct rstack *rpole;
 
